c663e1f33f94ab315b70297e151fb9163b8f9a11
pedx-ko <pedrxr2@gmail.com>
2023-06-07 15:43:04 +0000

    added root to docker group

--- install_docker_engine.sh
+++ install_docker_engine.sh
@@ -14,3 +14,6 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 #Verify that the Docker Engine installation is successful by running the hello-world image
 sudo docker run hello-world
+#we run docker as a root which is not recommended
+sudo groupadd docker
+sudo usermod -aG docker root
