d0a92d32a850ae00b84d35f287ce3af177e16d7f
pedx-ko <131887463+pedx-ko@users.noreply.github.com>
2023-06-07 17:20:24 +0200

    Docker installation works fine (#1)
    * Primer comentario
    
    * Inicio..
    
    * created install docker engine.sh

---
+++ install_docker_engine.sh
@@ -0,0 +1,16 @@
+# https://docs.docker.com/engine/install/ubuntu/#prerequisites
+#prerequisites (repo):
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#installing docker
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#Verify that the Docker Engine installation is successful by running the hello-world image
+sudo docker run hello-world
